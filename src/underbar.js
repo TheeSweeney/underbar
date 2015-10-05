297f98c78723416a2bd12a8162c43ee1549df9c0
Brian Sweeney <bsweeney930@gmail.com>
2015-10-05 15:31:18 -0400

    filter and reject done

--- src/underbar.js
+++ src/underbar.js
@@ -83,12 +83,26 @@
 
   // Return all elements of an array that pass a truth test.
   _.filter = function(collection, test) {
+    var arr = [];
+    for(var i=0; i<collection.length; i++){
+      if(test(collection[i])){
+        arr.push(collection[i])
+      }
+    }
+    return arr;
   };
 
   // Return all elements of an array that don't pass a truth test.
   _.reject = function(collection, test) {
     // TIP: see if you can re-use _.filter() here, without simply
     // copying code in and modifying it
+    var arr =[]
+    for(var i=0;i<collection.length; i++){
+      if(!test(collection[i])){
+        arr.push(collection[i])
+      }
+    }
+    return arr;
   };
 
   // Produce a duplicate-free version of the array.
