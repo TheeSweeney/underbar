01545088b5c8581d644d97af86cd5f78d7e17818
Brian Sweeney <bsweeney930@gmail.com>
2015-10-07 17:16:17 -0400

    every function using basic for loops passes all but one test: in the case of a conflict, it should use the last property's values when extending from multiple source objects. should probably use each, that may clear up issues.

--- src/underbar.js
+++ src/underbar.js
@@ -263,12 +263,35 @@
   //   }, {
   //     bla: "even more stuff"
   //   }); // obj1 now contains key1, key2, key3 and bla
-  _.extend = function(obj) {
+  _.extend = function(obj, input) {
+      if(arguments.length>2){
+        for(var i =1; i<arguments.length; i++){
+          for(var key in arguments[i]){
+            obj[key]= arguments[i][key]
+          }
+        }
+      }
+      for(var key in input){
+        obj[key] = input[key]
+      }
+      return obj;
   };
 
   // Like extend, but doesn't ever overwrite a key that already
   // exists in obj
-  _.defaults = function(obj) {
+  _.defaults = function(obj, input) {
+/*    if(arguments.length>2){
+        for(var i =1; i<arguments.length; i++){
+          for(var key in arguments[i]){
+            obj[key]= arguments[i][key]
+          }
+        }
+      }
+      for(var key in input){
+        obj[key] = input[key]
+      }
+      return obj;*/
+
   };
 
 
